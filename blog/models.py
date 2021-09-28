e5e82a7161c82012252b16f4019413d8891c2d57
C0012S <qkrtkd1002@duksung.ac.kr>
2021-09-29 01:25:25 +0900

    FBV로 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
+
+    def get_absolute_url(self): #self는 레코드 자기 자신을 의미하는 것
+        return f'/blog/{self.pk}'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from . models import Post
 
 # Create your views here.
+def index(request) :
+    posts = Post.objects.all().order_by('-pk') #pk의 역순으로 정렬
+
+    return render(request, 'blog/index.html',
+                  {
+                      'posts' : posts  #오른쪽의 posts가 위에서 정의한 posts를 의미
+                  }
+                  )
+
+def single_post_page(request, pk) :
+    post = Post.objects.get(pk=pk)  #= 중심으로 왼쪽은 Post 모델의 field 이름, 오른쪽은 그 field에 넣어 줄 해당되는 값(파라미터로 전달 받은 pk)
+
+    return render(request, 'blog/single_post_page.html',
+                  {
+                      'post' : post
+                  }
+                  )
