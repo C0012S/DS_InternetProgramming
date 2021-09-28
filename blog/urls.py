32df166ccc093fb00238af463413048ba968090c
C0012S <qkrtkd1002@duksung.ac.kr>
2021-09-29 04:06:17 +0900

    CBV로 블로그 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ # 서버IP/blog/
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),     # 서버IP/blog
+#    path('<int:pk>/', views.single_post_page), #view에 있는 함수 호출
+#    path('', views.index),     # 서버IP/blog
+
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()), #함수 대신 클래스 호출
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from . models import Post
 
 # Create your views here.
-def index(request) :
-    posts = Post.objects.all().order_by('-pk') #pk의 역순으로 정렬
+class PostList(ListView) :
+    model = Post
+    ordering = '-pk'
+#    template_name = 'blog/post_list.html'
+# post_list.html  #(index.html 이름을 post_list.html로 수정)  #template_name을 선언 안 해 주는 방법
 
-    return render(request, 'blog/index.html',
-                  {
-                      'posts' : posts  #오른쪽의 posts가 위에서 정의한 posts를 의미
-                  }
-                  )
+class PostDetail(DetailView) :
+    model = Post
+# post_detail.html  #(single_post_page.html 이름을 post_detail.html로 수정)
 
-def single_post_page(request, pk) :
-    post = Post.objects.get(pk=pk)  #= 중심으로 왼쪽은 Post 모델의 field 이름, 오른쪽은 그 field에 넣어 줄 해당되는 값(파라미터로 전달 받은 pk)
 
-    return render(request, 'blog/single_post_page.html',
-                  {
-                      'post' : post
-                  }
-                  )
+# def index(request) :
+#     posts = Post.objects.all().order_by('-pk') #pk의 역순으로 정렬
+#
+#     return render(request, 'blog/post_list.html',
+#                   {
+#                       'posts' : posts  #오른쪽의 posts가 위에서 정의한 posts를 의미
+#                   }
+#                   )
+
+# def single_post_page(request, pk) :
+#     post = Post.objects.get(pk=pk)  #= 중심으로 왼쪽은 Post 모델의 field 이름, 오른쪽은 그 field에 넣어 줄 해당되는 값(파라미터로 전달 받은 pk)
+#
+#     return render(request, 'blog/post_detail.html',
+#                   {
+#                       'post' : post
+#                   }
+#                   )
