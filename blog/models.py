0a4e0e526988cbd1e133adcd99ac02738b5c1b6f
C0012S <qkrtkd1002@duksung.ac.kr>
2021-10-12 13:17:51 +0900

    미리보기와 요약문 추가하기

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+import os
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
@@ -18,3 +20,9 @@ class Post(models.Model):
 
     def get_absolute_url(self): #self는 레코드 자기 자신을 의미하는 것
         return f'/blog/{self.pk}'
+
+    def get_file_name(self): #file 이름에 대한 부분 return
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self): #확장자 가져오는 함수
+        return self.get_file_name().split('.')[-1] #[-1]은 배열의 마지막 원소를 의미
